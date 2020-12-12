09d390ef41849b3508f3dd7970342410560ddb25
IgnacioManes <nacho.maness@gmail.com>
2020-12-12 17:38:23 -0300

    setup scope fix

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ import model.engine as me
 app = Flask(__name__)
 cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
+def _setup():
+    # FIXME: hardcoded, parse the real stuff from json/yaml
+    base_vars = ['mes', 'inv', 'lluvia']
+    target_var = 'x'
+    parsed_rules = [({'mes': 4, 'inv': "alto"}, {'x': "soja"},{'mes': "gt", 'inv': "eq"}),
+                    ({'mes': 4, 'lluvia': 3}, {'rb': 'si'},{'mes': "gt", 'lluvia': "leq"}),
+                    ({'mes': 4}, {'rb': 'si'},{'mes': "leq"}),
+                    ({'rb': "si",'inv':"bajo"}, {'x': "pasto"},{'rb': "eq",'inv':"eq"})]
+    questions = {'mes':'preg1','inv':'preg2','lluvia':'preg3'}
+    possible_values = {'mes':[1,2,3,4,5,6,7,8,9,10,11,12],
+                        'inv':['bajo','medio','alto'],
+                        'lluvia':[1,2,3]}
+    # init engine
+    eng = me.Engine(base_vars, target_var)
+    for p, q, ops in parsed_rules:
+        eng.add_rule(p, q, ops)
+    return eng,questions,possible_values
+
+eng, questions, possible_values = _setup()
+
 def present_next_var():
     nv = eng.get_next_var()
     pv = possible_values[nv]
@@ -44,25 +64,7 @@ def state():
     return {k: v for k, v in eng.model.items()}
 
 
-def _setup():
-    # FIXME: hardcoded, parse the real stuff from json/yaml
-    base_vars = ['mes', 'inv', 'lluvia']
-    target_var = 'x'
-    parsed_rules = [({'mes': 4, 'inv': "alto"}, {'x': "soja"},{'mes': "gt", 'inv': "eq"}),
-                    ({'mes': 4, 'lluvia': 3}, {'rb': 'si'},{'mes': "gt", 'lluvia': "leq"}),
-                    ({'mes': 4}, {'rb': 'si'},{'mes': "leq"}),
-                    ({'rb': "si",'inv':"bajo"}, {'x': "pasto"},{'rb': "eq",'inv':"eq"})]
-    questions = {'mes':'preg1','inv':'preg2','lluvia':'preg3'}
-    possible_values = {'mes':[1,2,3,4,5,6,7,8,9,10,11,12],
-                        'inv':['bajo','medio','alto'],
-                        'lluvia':[1,2,3]}
-    # init engine
-    eng = me.Engine(base_vars, target_var)
-    for p, q, ops in parsed_rules:
-        eng.add_rule(p, q, ops)
-    return eng,questions,possible_values
 
 
 if __name__ == '__main__':
-    eng,questions,possible_values = _setup()
     app.run(debug=True, port=5000)
